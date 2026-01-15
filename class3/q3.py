1e5e619bafe75faa20ae5b70be49a8a9fc475454
Molotov <16390267+molotov0coaktail@user.noreply.gitee.com>
2026-01-15 15:21:03 +0800

    Q3:添加了for循环并基本完成了程序。循环设置为一千次以避免死循环并便于后续调整和优化。

--- class3/q3.py
+++ class3/q3.py
@@ -3,3 +3,13 @@ prime2=11527
 N=32
 seed=eval(input("Please input a number"))
 Sequence=set()
+for i in range(1000):
+    seed*=prime1
+    seed+=prime2
+    seed%=2**32
+    if seed in Sequence:
+        break
+    else:
+        Sequence.add(seed)
+print(len(Sequence))
+print(Sequence)
