5438bf4448ba1f9538cfaf370b7aa87e3c4c5b45
Molotov <16390267+molotov0coaktail@user.noreply.gitee.com>
2026-01-16 22:54:03 +0800

    Q5:设置了循环以及对应的判断条件以对不同位置子图分别绘制散点图和直方图。

--- class5/q5.py
+++ class5/q5.py
@@ -9,3 +9,10 @@ petal_length=iris[:,2]
 petal_width=iris[:,3]
 
 fig,ax=plt.subplots(4,4)
+for i in range(4):
+    for j in range(4):
+        if i==j:
+            ax[i][j].hist(iris[:,j],bins=11)
+        else:
+            ax[i][j].scatter(iris[:,j],iris[:,i],s=10)
+plt.show()
