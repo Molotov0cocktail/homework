32097ae0bb1239274a11dc0724de5f87c3d23059
Molotov <16390267+molotov0coaktail@user.noreply.gitee.com>
2026-01-17 00:13:53 +0800

    Q6:创建了空字典用于存贮，并利用循环逐行将键值对添加在字典中

--- class6/q6.py
+++ class6/q6.py
@@ -1,4 +1,8 @@
+sysdirt={}
 with open('./class6/test.ini','rt',encoding='utf-8') as F:
     next(F)
     for line in F:
-        print(line)
+        a=line.strip().split('=')
+        print(a)
+        sysdirt[a[0]]=a[1]
+print(sysdirt)
