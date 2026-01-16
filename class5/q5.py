f4d198140e156e3b9fcdea29545cb7c57ee283bc
Molotov <16390267+molotov0coaktail@user.noreply.gitee.com>
2026-01-16 22:10:46 +0800

    Q5:import所有需要使用的宏包以及iris数据，并将数据分为四组

---
+++ class5/q5.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_iris
+import numpy as np
+import matplotlib.pyplot as plt
+
+iris = load_iris()['data']
+sepal_length=iris[:,0]
+sepal_width=iris[:,1]
+petal_length=iris[:,2]
+petal_width=iris[:,3]
