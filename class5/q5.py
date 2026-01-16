a89f99eeb3296dfce9c22c037dab928821a77317
Molotov <16390267+molotov0coaktail@user.noreply.gitee.com>
2026-01-16 22:55:15 +0800

    Q5:注释掉了没用上的分组

--- class5/q5.py
+++ class5/q5.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 iris = load_iris()['data']
-sepal_length=iris[:,0]
-sepal_width=iris[:,1]
-petal_length=iris[:,2]
-petal_width=iris[:,3]
+# sepal_length=iris[:,0]
+# sepal_width=iris[:,1]
+# petal_length=iris[:,2]
+# petal_width=iris[:,3]
 
 fig,ax=plt.subplots(4,4)
 for i in range(4):
