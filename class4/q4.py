98ef29ae27bd3723d8e3717ec1b046ba1cdbd1ba
Molotov <16390267+molotov0coaktail@user.noreply.gitee.com>
2026-01-16 19:28:33 +0800

    Q4:使用zeros创建空数组，并利用高级索引的方式形成One-hot Encoding

--- class4/q4.py
+++ class4/q4.py
@@ -1,5 +1,8 @@
 import numpy as np
 Class=10
 N=eval(input('Please input the number of sample'))
-encoding=np.random.randint(0,Class,size=N)
-# print(encoding)
+raw_parameter=np.random.randint(0,Class,size=N)
+# print(raw_parameter)
+onehot_encoding=np.zeros((N,Class),dtype=np.int32)
+onehot_encoding[np.arange(N),raw_parameter]=1
+print(onehot_encoding)
