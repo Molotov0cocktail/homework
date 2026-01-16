71dbd41501b017afe5163880cfc0258847ee5fec
Molotov <16390267+molotov0coaktail@user.noreply.gitee.com>
2026-01-17 00:04:37 +0800

    Q6:简化代码，用next函数直接跳过第一行

--- class6/q6.py
+++ class6/q6.py
@@ -1,6 +1,4 @@
 with open('./class6/test.ini','rt',encoding='utf-8') as F:
+    next(F)
     for line in F:
-        if line=='[languages]':
-            continue
-        else:
-            print(line)
+        print(line)
